730383662161fcad77d907a4043197e66fc0dbc5
sadunibhagya678-glitch <sadunibhagya678@gmail.com>
2026-01-29 23:19:10 +0530

    Add files via upload

---
+++ calculater.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def on_click(button_text):
+    current_text = entry.get()
+    if button_text == "=":
+        try:
+            # eval() calculates the string expression
+            result = eval(current_text)
+            enter.delete(0, tk.END)
+            enter.insert(tk.END, str(result))
+        except zeroDivisionError:
+            messagebox .showerror("Error","Cannot divide by zero")
+            enter.delete(0, tk.END)
+        except Exception:
+           messagebox.showerror("Error","invalid Input")
+           enter.delete(0, tk.END)
+    elif button_text == "C":
+           enter.delete(0, tk.END)
+    else:
+           entry.insert(tk.END, button_text)
